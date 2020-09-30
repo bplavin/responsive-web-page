780ea26891c99b0a07310f2f8a156a1289875e06
Bogdan Plavin <58284313+bplavin@users.noreply.github.com>
2020-09-29 22:55:40 -0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+"use strict";
+
+function openChat() {
+  document.getElementById("chat").style.display = "block";
+}
+
+function closeChat() {
+  document.getElementById("chat").style.display = "none";
+}
